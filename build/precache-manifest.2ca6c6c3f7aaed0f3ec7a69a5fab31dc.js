01c5ca53761c73b569082ed23401be637a5e75bc
Utsavladia <utsavladia@gmail.com>
2023-09-07 23:13:08 +0530

    Updated website content

---
+++ build/precache-manifest.2ca6c6c3f7aaed0f3ec7a69a5fab31dc.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021cb297ba37cb95fb2a1b0e9450570f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d0dc14f3a7f80aeb02e",
+    "url": "/static/css/main.0d923f67.chunk.css"
+  },
+  {
+    "revision": "7ad04c65912ca3f59977",
+    "url": "/static/js/2.5dd78c10.chunk.js"
+  },
+  {
+    "revision": "d21b4bc0702397bf916425d3b382139b",
+    "url": "/static/js/2.5dd78c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0dc14f3a7f80aeb02e",
+    "url": "/static/js/main.b6b895d7.chunk.js"
+  },
+  {
+    "revision": "24502419d788b7368fcb",
+    "url": "/static/js/runtime-main.cab49317.js"
+  },
+  {
+    "revision": "895577c652f537626ecc42280c1ae3fd",
+    "url": "/static/media/Black-Forest-Cakes_web.895577c6.webp"
+  },
+  {
+    "revision": "5e4e595b95f7c810123bea47e0374a47",
+    "url": "/static/media/Butterscotch-Cakes_web.5e4e595b.webp"
+  },
+  {
+    "revision": "1eb83b311d4b4d266df2b406903a4c3a",
+    "url": "/static/media/Dollcake.1eb83b31.png"
+  },
+  {
+    "revision": "f28eb71f966bb134d955424eda3caccb",
+    "url": "/static/media/Fresh-Fruit-Cakes_web.f28eb71f.webp"
+  },
+  {
+    "revision": "32040ef2720ed077c8a2c9352f4ee17f",
+    "url": "/static/media/Photocake.32040ef2.png"
+  },
+  {
+    "revision": "0c8191ad7c4b507cc79294d8af86c7d8",
+    "url": "/static/media/Pineapple-Cakes_web.0c8191ad.webp"
+  },
+  {
+    "revision": "09cbaa82a48decf9b992a0a0a31e892f",
+    "url": "/static/media/Red-Velvet-Cakes_web.09cbaa82.webp"
+  },
+  {
+    "revision": "819765705eceaf3d01f2ac193d9ce136",
+    "url": "/static/media/Truffle-Cakes_web.81976570.webp"
+  },
+  {
+    "revision": "5674f02594f91d75aeb608c550a598c1",
+    "url": "/static/media/Vanila-Cakes_web.5674f025.webp"
+  },
+  {
+    "revision": "e229a8e5a8bdd711136927d25505409f",
+    "url": "/static/media/bombcake.e229a8e5.png"
+  },
+  {
+    "revision": "60588198506a1f65260dd3b2f6eec09b",
+    "url": "/static/media/butterscotch_3.60588198.jpg"
+  },
+  {
+    "revision": "42f9efb67f398a9d8dfacaec1cf2a20c",
+    "url": "/static/media/choco_chips.42f9efb6.jpg"
+  },
+  {
+    "revision": "51e1ccdb8aa804fdd01f36028302ae5d",
+    "url": "/static/media/chocolate_loaded.51e1ccdb.jpg"
+  },
+  {
+    "revision": "47342464c064d207dfb339e564157e55",
+    "url": "/static/media/kitkat.47342464.jpg"
+  },
+  {
+    "revision": "1cc4f877c37c54d3b52e402c5652dc89",
+    "url": "/static/media/lavender_vanila.1cc4f877.jpg"
+  },
+  {
+    "revision": "056dfa3ca2a40784c8287dcc4361a78f",
+    "url": "/static/media/pinatacake1.056dfa3c.jpg"
+  },
+  {
+    "revision": "14baf4a2a3d5c661ece63aeaaf136b8c",
+    "url": "/static/media/pink_doll.14baf4a2.jpg"
+  },
+  {
+    "revision": "c37618d215756c1090bd9691c0a94e04",
+    "url": "/static/media/rasmalai.c37618d2.jpg"
+  },
+  {
+    "revision": "cdd03fc7e1b14731fd253c70cb1d9861",
+    "url": "/static/media/spider_1.cdd03fc7.jpg"
+  },
+  {
+    "revision": "fddb6b70dc477d562a37153e9f80d7cc",
+    "url": "/static/media/yellowcake.fddb6b70.jpeg"
+  }
+]);
