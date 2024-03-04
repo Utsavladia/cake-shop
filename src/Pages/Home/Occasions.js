d149e95ac409c7024b454238a497d9e52fabb950
Utsavladia <124615886+Utsavladia@users.noreply.github.com>
2024-03-04 21:01:58 +0530

    Update Occasions.js

--- src/Pages/Home/Occasions.js
+++ src/Pages/Home/Occasions.js
@@ -1,16 +1,16 @@
 export const Occasion = [
   {
-    title: "Birthday",
-    url: "../Cakes",
-    image:
-      "https://images.pexels.com/photos/7099900/pexels-photo-7099900.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
-    alt: "anniversary image"
-  },
-  {
     title: "Anniversary",
     url: "../Cakes",
     image:
       "https://images.unsplash.com/photo-1482575832494-771f74bf6857?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    alt: "anniversary image"
+  },
+  {
+    title: "Birthday",
+    url: "../Cakes",
+    image:
+      "https://images.unsplash.com/photo-1509666537727-9154b6962292?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "Birthday image"
   }
 ];
